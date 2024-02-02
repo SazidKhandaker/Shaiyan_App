75a48f3dc8aec9464fe748be6933a32d0d6fc5b1
unknown <sazid.khandaker@northsouth.edu>
2024-02-03 01:00:20 +0600

    SplashScreen done

---
+++ lib/Autantation/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginAuth extends StatefulWidget {
+  const LoginAuth({super.key});
+
+  @override
+  State<LoginAuth> createState() => _LoginAuthState();
+}
+
+class _LoginAuthState extends State<LoginAuth> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/splash/splash.dart
+++ lib/splash/splash.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:shaiyan_app/Autantation/login.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
+  SplashScreen({super.key});
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
@@ -9,7 +12,33 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => LoginAuth())));
+  }
+
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: NetworkImage(
+                  "https://images.unsplash.com/photo-1633354998322-415842c7ee11?q=80&w=2053&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+              fit: BoxFit.cover),
+        ),
+        child: Center(
+          child: Text(
+            "Welcome To ChatApp",
+            style: TextStyle(
+                color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
   }
 }
